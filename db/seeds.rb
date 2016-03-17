79be401b1428a8c2c12b3dbe04ac8cf3e3990a42
mattcheang <cheangmatt@gmail.com>
2016-03-17 14:41:04 -0700

    checkpoint-34-topics

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,20 @@
 require 'random_data'
 
+15.times do
+   Topic.create!(
+     name:         RandomData.random_sentence,
+     description:  RandomData.random_paragraph
+   )
+ end
+ topics = Topic.all
+
 
  50.times do
 
    Post.create!(
-
-     title:  RandomData.random_sentence,
-     body:   RandomData.random_paragraph
+    topic:  topics.sample,
+    title:  RandomData.random_sentence,
+    body:   RandomData.random_paragraph
    )
  end
  posts = Post.all
@@ -14,12 +22,13 @@ require 'random_data'
 
  100.times do
    Comment.create!(
- 
+
      post: posts.sample,
      body: RandomData.random_paragraph
    )
  end
 
  puts "Seed finished"
+ puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
