74ca30f3ec7fa39616b4d853c45cebb7104c85a6
mattcheang <cheangmatt@gmail.com>
2016-03-23 19:53:25 -0700

    checkpoint-38-signin: completed

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -28,7 +28,7 @@ RSpec.describe UsersController, type: :controller do
        expect(response).to have_http_status(:redirect)
      end
 
- 
+
      it "creates a new user" do
        expect{
          post :create, user: new_user_attributes
@@ -58,5 +58,10 @@ RSpec.describe UsersController, type: :controller do
        post :create, user: new_user_attributes
        expect(assigns(:user).password_confirmation).to eq new_user_attributes[:password_confirmation]
      end
-   end
+
+     it "logs the user in after sign up" do
+      post :create, user: new_user_attributes
+      expect(session[:user_id]).to eq assigns(:user).id
+    end
+  end
 end
