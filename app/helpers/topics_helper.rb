7621bee3b7f9ac575086675b327350223576b193
mattcheang <cheangmatt@gmail.com>
2017-02-02 14:23:10 -0800

    update scope

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,9 +1,5 @@
 module TopicsHelper
   def user_is_authorized_for_topics?
-      current_user.nil? || current_user || current_user.admin?
-  end
-
-  def topic_authorized?
-    current_user == @topic.user || current_user.admin?
+      current_user.nil? == false
   end
 end
