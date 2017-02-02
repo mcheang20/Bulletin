660ae3d0bbc473932d469791633adefe17a9e24e
mattcheang <cheangmatt@gmail.com>
2017-02-01 20:55:10 -0800

    fix edit topic

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -2,4 +2,8 @@ module TopicsHelper
   def user_is_authorized_for_topics?
       current_user.nil? || current_user || current_user.admin?
   end
+
+  def topic_authorized?
+      current_user && (current_user == @topic.user || current_user.admin?)
+  end
 end
