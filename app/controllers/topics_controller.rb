d2f14ee9e89924089e78eb734d4d7ae2d14e9007
mattcheang <cheangmatt@gmail.com>
2017-02-01 22:36:07 -0800

    update topics controller

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,8 +2,6 @@ class TopicsController < ApplicationController
 
   before_action :require_sign_in, except: [:index, :show]
 
-  before_action :authorize_user, except: [:index, :show, :new, :create]
-
   def index
     @topics = Topic.all
     if params[:search]
@@ -73,13 +71,6 @@ class TopicsController < ApplicationController
   private
 
   def topic_params
-    params.require(:topic).permit(:name, :description, :public)
-  end
-
-  def authorize_user
-    unless current_user || current_user.admin?
-      flash[:alert] = "You must be an admin to do that."
-      redirect_to topics_path
-    end
+    params.require(:topic).permit(:name, :description, :public, :user_id)
   end
 end
