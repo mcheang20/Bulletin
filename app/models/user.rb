f3376771743f6b074fe5b5e9b25b277ebec5e07c
mattcheang <cheangmatt@gmail.com>
2017-02-01 21:57:24 -0800

    fix topic user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :topics, dependent: :destroy
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
